12f18dad731498f009ea24b0724e1aa327da3d62
ShinelonMarcLei <estupidoc@163.com>
2023-08-16 23:28:32 +0800

    add generics v2

--- 2_generics_test.go
+++ 2_generics_test.go
@@ -13,7 +13,7 @@ func getKeys[K comparable, V any](m map[K]V) []K {
 	return keys
 }
 
-func Test_generics(t *testing.T) {
+func Test_generics_v1(t *testing.T) {
 	m1 := map[int]string{
 		1: "a",
 		2: "b",
@@ -30,4 +30,41 @@ func Test_generics(t *testing.T) {
 	fmt.Println(getKeys(m2))
 	fmt.Println(getKeys[int, string](m1))
 	fmt.Println(getKeys[string, int](m2))
+}
+
+type element[T any] struct {
+	next *element[T]
+	val  T
+}
+
+type List[T any] struct {
+	head, tail *element[T]
+}
+
+func (lst *List[T]) push(v T) {
+	if lst.tail == nil {
+		lst.head = &element[T]{val: v}
+		lst.tail = lst.head
+	} else {
+		lst.tail.next = &element[T]{val: v}
+		lst.tail = lst.tail.next
+	}
+}
+
+func (lst *List[T]) getAll() []T {
+	var elems []T
+	for e := lst.head; e != nil; e = e.next {
+		elems = append(elems, e.val)
+	}
+	return elems
+}
+
+func Test_generics_v2(t *testing.T) {
+	var lst List[int]
+	lst.push(1)
+	lst.push(12)
+	lst.push(123)
+	lst.push(1234)
+	lst.push(12345)
+	fmt.Println(lst.getAll())
 }
