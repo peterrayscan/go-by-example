8f76b2906a580a647da90bafd021faad88d7ffe4
ShinelonMarcLei <estupidoc@163.com>
2023-08-21 23:22:33 +0800

    add channel sync.

--- 7_channel_sync_test.go
+++ 7_channel_sync_test.go
@@ -1,7 +1,21 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func work(ch chan bool) {
+	fmt.Println("working...")
+	time.Sleep(1 * time.Second)
+	fmt.Println("done.")
+	ch <- true
+}
 
 func Test_channel_sync(t *testing.T) {
-
+	do := make(chan bool)
+	go work(do)
+	<-do
+	fmt.Println("work has been finished.")
 }
---
+++ 8_channel_directions_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_channel_directions(t *testing.T) {
+
+}
