dd6b6a8117e27a410adf2616aaaabe94ede103f7
Peter Rays <estupidoc@163.com>
2023-08-22 09:15:32 +0000

    add channel direction.

--- 8_channel_directions_test.go
+++ 8_channel_directions_test.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
-func Test_channel_directions(t *testing.T) {
+func set(ch chan<- string, msg string) {
+	ch <- msg
+}
 
+func get(ch <-chan string) string {
+	return <-ch
+}
+
+func Test_channel_directions(t *testing.T) {
+	ch := make(chan string, 1)
+	set(ch, "this is a message.")
+	fmt.Println("got:", get(ch))
 }
