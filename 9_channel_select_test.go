e1a49bf199a871199da2b4dc43753bfeb791b48f
Peter Rays <estupidoc@163.com>
2023-08-23 02:52:46 +0000

    add channel select.

--- 9_channel_select_test.go
+++ 9_channel_select_test.go
@@ -1,7 +1,31 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
 func Test_channel_select(t *testing.T) {
+	ch1 := make(chan string)
+	ch2 := make(chan string)
 
+	go func() {
+		time.Sleep(1 * time.Second)
+		ch1 <- "done after 1 second."
+	}()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch2 <- "done after 2 seconds."
+	}()
+
+	for i := 0; i < 2; i++ {
+		select {
+		case msg := <-ch1:
+			fmt.Println("got:", msg)
+		case msg := <-ch2:
+			fmt.Println("got:", msg)
+		}
+	}
 }
