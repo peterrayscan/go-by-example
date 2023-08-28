ce7a0195d9cc687f1a6f78c9f24ff7efa546699f
Peter Rays <estupidoc@163.com>
2023-08-28 05:05:34 +0000

    add timer.

--- 14_timer_test.go
+++ 14_timer_test.go
@@ -1,7 +1,28 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
 func Test_timer(t *testing.T) {
+	timer1 := time.NewTimer(2 * time.Second)
+	<-timer1.C
+	fmt.Println("timer1 is fired.")
 
+	timer2 := time.NewTimer(1 * time.Second)
+	go func() {
+		<-timer2.C
+		fmt.Println("timer2 is fired.")
+	}()
+
+	// in some conditiion, you may want to stop the timer before it fired.
+	if true {
+		success := timer2.Stop()
+		if success {
+			fmt.Println("timer2 is stopped.")
+		}
+	}
+	time.Sleep(2 * time.Second)
 }
---
+++ 15_ticker_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func Test_ticker(t *testing.T) {
+
+}
